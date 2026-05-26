1f4abd875045b1735b544ec69a8c16c7544f382a
Ahmed Bahaa <ahmed@yama-sushi.com>
2026-05-26 04:53:06 +0300

    feat: Tawk.to live chat widget
    - Added Tawk.to live chat script to [locale]/layout.tsx head
    - Enables real-time support chat for visitors
    - Helps with sales conversions and support inquiries

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -91,6 +91,21 @@ export default async function LocaleLayout({
             }),
           }}
         />
+        {/* Tawk.to Live Chat */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6651e2e29a809f19fb3765b3/1hvi8i6gm';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();`.trim(),
+          }}
+        />
       </head>
       <body className={`min-h-screen flex flex-col font-sans antialiased page-enter`}>
         <NextIntlClientProvider messages={messages}>
